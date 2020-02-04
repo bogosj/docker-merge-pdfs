39fda2fcb28e8fed97dc85577e40bbb1168c2fa2
James Bogosian <bogosj@gmail.com>
2020-02-04 23:40:10 +0000

    Write temp output to a temp directory.

--- merge-pdfs.sh
+++ merge-pdfs.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-set -x #echo on
 
-pdftk /work/files/*pdf cat output /work/files/input.pdf
+TMP_DIR=`mktemp -d`
+
+pdftk /work/files/*pdf cat output $TMP_DIR/input.pdf
 gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/default \
-         -dNOPAUSE -dQUIET -dBATCH -sOutputFile=/work/files/output.pdf /work/files/input.pdf
-rm /work/files/input.pdf
+         -dNOPAUSE -dQUIET -dBATCH -sOutputFile=/work/files/output.pdf $TMP_DIR/input.pdf
 
